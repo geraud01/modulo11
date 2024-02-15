6c9cdee12aa6c594c0f81931f738ab55ec83c9a5
Geraud Oliveira <geraud.pereira@hotmail.com>
2024-02-15 15:09:48 -0300

    Intação de bibliotecas e dependencias

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Menu from './components/Menu';
+import Home from './components/Home';
+import Cadastro from './components/Cadastro';
+import Listagem from './components/Listagem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Menu />
+      <Routes>
+        <Route element={<Home />} path="/home" />
+        <Route element={<Cadastro />} path="/cadastro" />
+        <Route element={<Listagem />} path="/listagem" />
+      </Routes>
+    </Router>
   );
 }
 
